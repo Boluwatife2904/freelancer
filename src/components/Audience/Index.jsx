410ac4d3a0a43123e413f5029192318da7c4ce26
Boluwatife2904 <sanvicola2000@gmail.com>
2024-11-02 15:31:09 +0100

    fix: overlapping containers after audience section animation

--- src/components/Audience/Index.jsx
+++ src/components/Audience/Index.jsx
@@ -20,8 +20,7 @@ function Audience() {
                 end: "bottom top",
                 scrub: 1,
                 pin: ".audience",
-                toggleActions: "play complete complete complete",
-                // once: true,
+                toggleActions: "play none none none",
             },
         });
         timeline
@@ -33,7 +32,7 @@ function Audience() {
     }, []);
 
     return (
-        <section className="audience bg-[#FE5824] min-h-screen flex items-center justify-center text-center py-[225px] overflow-hidden">
+        <section className="audience bg-[#FE5824] min-h-screen flex items-center justify-center text-center py-[225px]">
             <div className="max-w-[516px] lg:max-w-[858px] mx-auto px-5 relative">
                 <h6 className="text-white audience-title font-medium text-[46px] leading-[54px] sm:text-[52px] md:text-[60px] lg:text-[120px] lg:leading-[106px] -tracking-[2%]">
                     Open doors, diverse minds: welcomes all
